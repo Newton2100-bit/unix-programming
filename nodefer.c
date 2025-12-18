c7e682f9be1aab6df2f025fab623174c8c3f3912
newton irungu <mwauranewton99@gmail.com>
2025-12-18 21:34:24 +0300

    the nodefer option in siagction flags

---
+++ nodefer.c
@@ -0,0 +1,20 @@
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void handler(int sig) {
+	char msg1[] = "\rHandler start\n", msg2[] = "Handler end\n";
+	write(2, msg1, sizeof(msg1));
+	sleep(2);
+	write(2, msg2, sizeof(msg2));
+}
+
+int main() {
+	struct sigaction sa = {0};
+	sa.sa_handler = handler;
+	sigemptyset(&sa.sa_mask);  // no extra blocked signals
+				   // sa.sa_flags = 0;         // default behavior
+	sigaction(SIGINT, &sa, NULL);
+
+	for (;;) pause();
+}
