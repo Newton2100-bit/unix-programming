797f9797061f4aaff6d1be3fc87b23b7afce5a8f
newton irungu <mwauranewton99@gmail.com>
2025-12-22 08:21:25 +0300

    a simple refactor

--- nodefer.c
+++ nodefer.c
@@ -2,14 +2,16 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void handler(int sig) {
+void
+handler(int sig) {
 	char msg1[] = "\rHandler start\n", msg2[] = "Handler end\n";
 	write(2, msg1, sizeof(msg1));
 	sleep(2);
 	write(2, msg2, sizeof(msg2));
 }
 
-int main() {
+int
+main() {
 	struct sigaction sa = {0};
 	sa.sa_handler = handler;
 	sigemptyset(&sa.sa_mask);  // no extra blocked signals
